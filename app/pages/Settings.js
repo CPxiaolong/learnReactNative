e4d2073619f6f53f570074c2318b681654c57d42
v_wangxiaolong <v_wangxiaolong01@baidu.com>
2018-12-07 23:56:21 +0800

    fent(设置页): 把设置页添加了scrolltap组件

--- app/pages/Settings.js
+++ app/pages/Settings.js
@@ -9,16 +9,21 @@ import { View, Text, Button, Image, StyleSheet} from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { createBottomTabNavigator, createAppContainer, SafeAreaView } from "react-navigation";
+import ScrollableTabView, {DefaultTabBar, } from 'react-native-scrollable-tab-view';
 
 export default class SettingsScreen extends React.Component {
     static navigationOptions = ({ navigation }) => ({
         header: null,
     });
     render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Settings!</Text>
-            </View>
-        );
+        return <ScrollableTabView
+            style={{marginTop: 20, }}
+            initialPage={1}
+            renderTabBar={() => <DefaultTabBar />}
+        >
+            <Text tabLabel='Tab #1'>My</Text>
+            <Text tabLabel='Tab #2'>favorite</Text>
+            <Text tabLabel='Tab #3'>project</Text>
+        </ScrollableTabView>;
     }
 }
