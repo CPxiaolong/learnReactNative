7f5a603eb0a8aa7f5076dcaf848a2b77c6258a35
v_wangxiaolong <v_wangxiaolong01@baidu.com>
2018-12-14 15:07:32 +0800

    feat(react-native-video): 添加成功

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -9,6 +9,7 @@ import { Image, StyleSheet, View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import InfoFlatList from '../component/InfoFlatList/index';
 import axios from "axios";
+import Video from 'react-native-video';
 export default class HomeScreen extends Component {
 
     constructor(props) {
@@ -16,25 +17,40 @@ export default class HomeScreen extends Component {
         this.state = { 
             movieList: [],
         };
+        this.onBuffer = this.onBuffer.bind(this)
+        this.videoError = this.videoError.bind(this)
     }
 
     componentDidMount() {  
     }
 
+    onBuffer() {
+
+    }
+
+    videoError() {
+
+    }
     render() {
         return (
-            <InfoFlatList
-                onPress = {(url) => 
-                    this.props.navigation.navigate('Details', {
-                        url
-                    })
-                }
-            />
+            <Video source={{uri: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"}}   // Can be a URL or a local file.
+            ref={(ref) => {
+              this.player = ref
+            }}                                      // Store reference
+            onBuffer={this.onBuffer}                // Callback when remote video is buffering
+            onError={this.videoError}               // Callback when video cannot be loaded
+            style={styles.backgroundVideo} />
                         
         );
     }
 }
 
 const styles = StyleSheet.create({
-    
+    backgroundVideo: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    },
 });
